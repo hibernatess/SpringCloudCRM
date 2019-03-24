c21976fa7cea457dca2614a557cbb789099e3e0f
hibernatess <1959227206@qq.com>
2019-03-24 16:28:34 +0800

    初始化

--- SpringCloudCRM-consumer-crm-80/src/main/java/com/ywj/springcloud/controller/UserController.java
+++ SpringCloudCRM-consumer-crm-80/src/main/java/com/ywj/springcloud/controller/UserController.java
@@ -5,6 +5,7 @@ import com.ywj.springcloud.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -13,14 +14,16 @@ import org.springframework.web.client.RestTemplate;
  * @Describe
  * @Date 2019/3/24 0024 15:51
  */
-@Controller
+@RestController
 public class UserController {
+//    private static  final String REST_URL_PREFIX="http://SPRINGCLOUDCRM-LOGIN";
+private static  final String REST_URL_PREFIX="http://localhost:8002";
     @Autowired
     private RestTemplate restTemplate;
 
-    @RequestMapping("/login")
+    @RequestMapping("/consumer/login")
     public User loadUserLogin(User user){
-        return restTemplate.getForObject("",User.class);
+        return restTemplate.getForObject(REST_URL_PREFIX+"/login?usr_name="+user.getUsr_name()+"&usr_password="+user.getUsr_password(),User.class);
     }
 
 }
