47b35835b5a8fc6f291f40107b82fdfdde401e0c
e <2548152658@qq.com>
2019-03-24 09:28:25 +0800

    hhh

---
+++ SpringCloudCRM-provider-server-8003/src/main/java/com/ywj/springlcoud/Server8003App.java
@@ -0,0 +1,11 @@
+package com.ywj.springlcoud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Server8003App {
+    public static void main(String[] args) {
+        SpringApplication.run(Server8003App.class,args);
+    }
+}
