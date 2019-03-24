a0f3ae63bfcb0f58b360a0b8c9ea7f2fd6891c54
e <2548152658@qq.com>
2019-03-24 15:50:16 +0800

    hhh

---
+++ SpringCloudCRM-consumer-crm-80/src/main/java/com/ywj/springcloud/controller/ServiceController.java
@@ -0,0 +1,28 @@
+package com.ywj.springcloud.controller;
+
+import com.ywj.springcloud.config.ConfigBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+* @Description:    SpringCloudCRM-provider-server-8003的消费端
+* @Author:         hhh
+* @CreateDate:
+* @UpdateUser:     hhh
+* @UpdateDate:
+*/
+@RestController
+public class ServiceController {
+
+    @Autowired
+    private ConfigBean configBean;
+
+
+
+
+
+
+
+}
