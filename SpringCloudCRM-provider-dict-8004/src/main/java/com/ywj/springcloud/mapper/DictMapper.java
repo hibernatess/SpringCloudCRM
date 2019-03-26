44d88748f29cb23f69f2f40271983be8ba94bbbd
hibernatess <1959227206@qq.com>
2019-03-26 16:03:29 +0800

    初始化

---
+++ SpringCloudCRM-provider-dict-8004/src/main/java/com/ywj/springcloud/mapper/DictMapper.java
@@ -0,0 +1,17 @@
+package com.ywj.springcloud.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface DictMapper {
+
+
+
+
+
+
+}
