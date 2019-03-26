3a699d069a61c8334b1f0220c2bd35a1b3b52b4e
hibernatess <1959227206@qq.com>
2019-03-26 09:54:26 +0800

    初始化

--- SpringCloudCRM-consumer-crm-80/src/main/java/com/ywj/springcloud/config/ConfigBean.java
+++ SpringCloudCRM-consumer-crm-80/src/main/java/com/ywj/springcloud/config/ConfigBean.java
@@ -4,6 +4,9 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @ClassName ConfigBean
@@ -12,9 +15,9 @@ import org.springframework.web.client.RestTemplate;
  * @Date 2019/3/24 0024 10:06
  */
 @Configuration
-public class ConfigBean {
- /**    
-     *@Description: 用于调用其他服务
+public class ConfigBean  {
+    /**
+     * @Description: 用于调用其他服务
      * @Param: []
      * @return: org.springframework.web.client.RestTemplate
      * @Author: ywj
@@ -26,7 +29,7 @@ public class ConfigBean {
         return new RestTemplate();
     }
 
-    
-    
-    
+
+
+
 }
--- SpringCloudCRM-consumer-crm-80/src/main/java/com/ywj/springcloud/controller/UserController.java
+++ SpringCloudCRM-consumer-crm-80/src/main/java/com/ywj/springcloud/controller/UserController.java
@@ -4,9 +4,14 @@ package com.ywj.springcloud.controller;
 import com.ywj.springcloud.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * @ClassName UserController
@@ -14,15 +19,25 @@ import org.springframework.web.client.RestTemplate;
  * @Describe
  * @Date 2019/3/24 0024 15:51
  */
-@RestController
+@Controller
 public class UserController {
-    private static  final String REST_URL_PREFIX="http://SPRINGCLOUDCRM-LOGIN";
+    private static final String REST_URL_PREFIX = "http://SPRINGCLOUDCRM-LOGIN";
     @Autowired
     private RestTemplate restTemplate;
-
+    @ResponseBody
     @RequestMapping("/consumer/login")
-    public User loadUserLogin(User user){
-        return restTemplate.getForObject(REST_URL_PREFIX+"/login?usr_name="+user.getUsr_name()+"&usr_password="+user.getUsr_password(),User.class);
+    public Boolean loadUserLogin(User user, HttpSession session) {
+        User User = restTemplate.getForObject(REST_URL_PREFIX + "/login?usr_name=" + user.getUsr_name() + "&usr_password=" + user.getUsr_password(), User.class);
+        if (User != null) {
+            session.setAttribute("user",User);
+            return true;
+        }
+        return false;
+    }
+
+   @GetMapping("/")
+    public String loging(){
+        return  "login";
     }
 
 }
